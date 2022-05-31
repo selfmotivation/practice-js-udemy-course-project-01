f99762c3df1813ae4c49350aa512bd20f978a17c
Andrey Kiselev <mira55@yandex.ru>
2022-05-31 04:51:34 +0400

    feat: add showing modal by 60s

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,3 +1,5 @@
+import { setTimeout } from "core-js";
+
 const modals = () => {
   function bindModal(triggerSelector, modalSelector, closeSelector) {
     const trigger = document.querySelectorAll(triggerSelector);
@@ -11,32 +13,34 @@ const modals = () => {
         }
   
         modal.style.display = 'block';
-        // document.body.style.overflow = 'hidden';
         document.body.classList.add('modal-open');
       });
     });
 
     close.addEventListener('click', () => {
       modal.style.display = 'none';
-      // document.body.style.overflow = '';
       document.body.classList.remove('modal-open');
     });
 
     modal.addEventListener('click', (e) => {
       if (e.target === modal) {
         modal.style.display = 'none';
-        // document.body.style.overflow = '';
         document.body.classList.remove('modal-open');
       }
     })
   }
 
-  const callEngineerBtn = document.querySelector('.popup_engineer_btn');
-  const modalEngineer = document.querySelector('.popup_engineer');
-  const modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
+  function showModalByTime(selector, time) {
+    setTimeout(() => {
+      document.querySelector(selector).style.display = 'block';
+      document.body.classList.add('modal-open');
+    }, time);
+  }
 
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
   bindModal('.phone_link', '.popup', '.popup .popup_close');
+
+  showModalByTime('.popup', 3000);
 };
 
 export default modals;
